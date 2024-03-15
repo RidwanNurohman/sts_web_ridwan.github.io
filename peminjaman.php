69d0367de1509ff8249e5601a3e634ab07a4578e
RidwanNurohman <144863986+RidwanNurohman@users.noreply.github.com>
2024-03-15 13:49:57 +0700

    Update peminjaman.php

--- peminjaman.php
+++ peminjaman.php
@@ -15,6 +15,12 @@ $nomor = 0;
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+    /* Custom CSS */
+    #content-wrapper {
+      margin-left: 200px; /* Adjust this value according to your sidebar width */
+    }
+  </style>
 
   <title>Peminjaman</title>
 </head>
@@ -58,12 +64,14 @@ $nomor = 0;
     <thead>
         <tr>
             <th>No</th>
+            <th>ID</th>
             <th>No Identitas</th>
             <th>Kode Barang</th>
             <th>Jumlah</th>
             <th>Keperluan</th>
             <th>Status</th>
             <th>Tanggal Pinjam</th>
+            <th>ID Login</th>
             <th>Action</th>
             
         </tr>
@@ -73,12 +81,14 @@ $nomor = 0;
         <?php $nomor++; ?>
         <tr>
             <th scope="row"><?php echo "$nomor"; ?></th>
+            <td><?php echo "$peminjaman[id]";?></td>
             <td><?php echo "$peminjaman[no_identitas]";?></td>
             <td><?php echo "$peminjaman[kode_barang]";?></td>
             <td><?php echo "$peminjaman[jumlah]";?></td>
             <td><?php echo "$peminjaman[keperluan]";?></td>
             <td><?php echo "$peminjaman[status]";?></td>
             <td><?php echo "$peminjaman[tgl_pinjam]";?></td>
+            <td><?php echo "$peminjaman[id_login]";?></td>
             <td>
                 <?php
                     echo "
