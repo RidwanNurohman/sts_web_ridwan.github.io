6e1ca9fa37087d4b6fa88bedf9fc60a0d7497add
RidwanNurohman <144863986+RidwanNurohman@users.noreply.github.com>
2024-03-15 13:47:14 +0700

    Update home.php

--- home.php
+++ home.php
@@ -9,6 +9,13 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    
+    <style>
+    /* Custom CSS */
+    #content-wrapper {
+      margin-left: 200px; /* Adjust this value according to your sidebar width */
+    }
+  </style>
 
   <title>Home</title>
 </head>
@@ -40,7 +47,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
     -->
-<div class="container mt-5">
+<div class="container mr-4">
 
     <div class="row text-center">
       <div class="col-md-4">
@@ -86,7 +93,7 @@
                 ?>
 
                 <h5 class="card-title">Data Peminjaman</h5>
-                <p class="card-text">Jumlah barang yang dipinjam </p>
+                <p class="card-text">Jumlah user yang meminjam barang </p>
                 <h4><?= $peminjaman['jmlPinjam']; ?></h4>
                 <a href="peminjaman.php" class="btn btn-dark btn-block">Lihat data peminjaman</a>
              </div>
@@ -104,7 +111,7 @@
                 ?>
 
                 <h5 class="card-title">Data Pengembalian</h5>
-                <p class="card-text">Jumlah barang  dikembalikan </p>
+                <p class="card-text">Jumlah user yang mengembalikan barang </p>
                 <h4><?= $pengembalian['jmlKembali']; ?></h4>
                 <a href="pengembalian.php" class="btn btn-dark btn-block">Lihat data pengembalian</a>
              </div>
